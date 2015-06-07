dc547536c98a35ad113baa11a45d5ead117ccd74
JG <jg@example.com>
2015-06-07 12:00:15 -0400

    Fix python style issues

--- media.py
+++ media.py
@@ -2,8 +2,8 @@ import webbrowser
 
 class Movie():
     """This class defines a movie and key information about it."""
-    def __init__(self, movie_title, release_year, poster_image, trailer_youtube):
-        self.title = movie_title
+    def __init__(self, title, release_year, poster_image, trailer_youtube):
+        self.title = title
         self.release_year = release_year
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
--- movies_p1.py
+++ movies_p1.py
@@ -1,21 +1,22 @@
-#Movie Website - Project 1 for the Full Stack Web Developer Nanodegree 
-#import required modules
+# Movie Website - Project 1 for the Full Stack Web Developer Nanodegree 
+# import required modules
 import csv
-import media  #movie class
-import fresher_tomatoes #web page display generator
+import media  # movie class
+import fresher_tomatoes  # web page display generator
 
 movies = list()
 
-#load movie data from CSV file into list of Movie objects
+# load movie data from CSV file into list of Movie objects
 faves = open('fave_movies.csv')
 with faves as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-                fave = media.Movie(row['title'],row['release_year'],row['poster_url'],row['trailer_url'])
+                fave = media.Movie(row['title'],row['release_year'],
+                                   row['poster_url'],row['trailer_url'])
                 movies.append(fave)
                 
 faves.close()
 
-#build & display webpage
+# build & display webpage
 fresher_tomatoes.open_movies_page(movies)
 
